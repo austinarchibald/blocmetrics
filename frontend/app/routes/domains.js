a07ce1588a267f137056e766363479ff18cb508f
Austin Archibald <austin.archibald@gmail.com>
2015-06-30 14:47:08 -0700

    fixed session authentication persistence issues

--- frontend/app/routes/domains.js
+++ frontend/app/routes/domains.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() { return this.store.find("domain") },
   actions: {
     delete: function(domain) {
       var _this = this;
--- frontend/app/routes/domains/new.js
+++ frontend/app/routes/domains/new.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model: function() {
-		return this.store.createRecord('domain');
-	}
+  model: function() {
+    return this.store.createRecord('domain');
+  },
+  deactivate: function() {
+    var model  = this.modelFor('domains/new');
+    if (model.get('isNew')) {
+      model.destroyRecord();
+    }
+  }
 });
